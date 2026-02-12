adc73b891919dcfced1a1e4610f0ab9ef08c516d
Antik-Paul <antik.officials@gmail.com>
2026-02-12 11:39:06 +1000

    Final project for week 3

--- CP1401/Week_3/question_3.py
+++ CP1401/Week_3/question_3.py
@@ -1,1 +1,6 @@
-'''Couldn't Understand the question'''
+secret_password = "swordfish"
+guess = input("Enter the secret password: ")
+if guess == secret_password:
+    print("Access granted")
+else:  
+    print("Access denied")

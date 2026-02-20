039772271d132ef9be06b3d4627f7be2da13fd67
Antik-Paul <antik.officials@gmail.com>
2026-02-21 00:40:17 +1000

    code done for q1 of A1

--- CP1401/Assessment_1/a1_1_pizza_pay_calculator.py
+++ CP1401/Assessment_1/a1_1_pizza_pay_calculator.py
@@ -1,1 +1,16 @@
 print("Warm Pizza Pay Calculator")
+
+PAY_PER_TRIP = 1.45
+PAY_PER_MINUTE = 0.95
+
+number_of_trips = int(input("number of trips: "))
+number_of_minutes = int(input("number of minutes: "))
+
+total_pay_of_trips = PAY_PER_TRIP * number_of_trips
+total_pay_of_minutes = PAY_PER_MINUTE * number_of_minutes
+
+total_pay = total_pay_of_minutes + total_pay_of_trips
+
+print(f'For {number_of_trips} trips, your pay is: $ {total_pay_of_trips:.2f}')
+print(f'For {number_of_minutes} minutes, your pay is: $ {total_pay_of_minutes:.2f}')
+print(f'Your total pay is ${total_pay:.2f}')
---
+++ CP1401/Assessment_1/a1_2_tennis.py
@@ -0,0 +1,5 @@
+print("Welcome Player 1. How was your match?")
+
+player_1_score = int(input("Your score: "))
+opponent_score = int(input("Opponent score: "))
+
